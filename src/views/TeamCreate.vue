e8cd44234fd329c61c59ef853b2d2a4c8f1047c6
kusumoto <kusumoto@PC10312.local>
2018-08-11 02:00:28 +0900

    チーム作成画面: 招待メールの送信

--- src/views/TeamCreate.vue
+++ src/views/TeamCreate.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <input
+      v-model="teamName"
+      placeholder="チーム名を入力"
+    >
     <ul
-      v-for="(member, index) in invitingMembers"
+      v-for="(item, index) in invitingMembers"
       :key="index"
     >
       <li>
+        <p>
+          {{ item.message }}
+        </p>
         <input
-          v-model="member.email"
+          v-model="item.email"
           placeholder="メールアドレスを入力して招待"
         >
         <button
@@ -21,16 +28,33 @@
     >
       +
     </button>
-    <button>
+    <button
+      @click="onCreateTeamClicked"
+    >
       スクラムチーム作成
     </button>
   </div>
 </template>
 
 <script>
+import firebase from './../firebase'
+
+const db = firebase.firestore()
+const settings = {
+  timestampsInSnapshots: true
+}
+db.settings(settings)
+
+// メールアクションの設定
+const actionCodeSettings = {
+  url: 'http://localhost:8080/?finishSignUp?cartId=1234',
+  handleCodeInApp: true
+}
+
 export default {
   data: function () {
     return {
+      teamName: '',
       invitingMembers: []
     }
   },
@@ -38,12 +62,48 @@ export default {
     addInput: function () {
       this.invitingMembers.push(
         {
-          'email': ''
+          'email': '',
+          'isSucceed': false,
+          'message': ''
         }
       )
     },
     removeInput: function (index) {
       this.invitingMembers.splice(index, 1)
+    },
+    onCreateTeamClicked: function () {
+      // 1. create new ScrumTeams
+      // db.collection('ScrumTeams').add({
+      //   'teamName': this.teamName,
+      //   'config': {
+      //     'estimationUnit': 'story points',
+      //     'status': ['To Do', 'Doing', 'Done']
+      //   }
+      // })
+      //   .then(docRef => {
+      //
+      //   })
+      this.sendEmail()
+    },
+    sendEmail: function () {
+      this.invitingMembers.forEach((e, i) => {
+        if (!e.email) return
+        firebase.auth().sendSignInLinkToEmail(e.email, actionCodeSettings)
+          .then(() => {
+            this.invitingMembers[i].isSucceed = true
+            this.invitingMembers[i].message = '招待メールが送信されました。参加をお待ちください。'
+          })
+          .catch(error => {
+            this.invitingMembers[i].isSucceed = false
+            let errorMessage = ''
+            if (error.code === 'auth/invalid-email') {
+              errorMessage = 'メールアドレスの形式が正しくありません。'
+            } else {
+              errorMessage = error.message
+            }
+            this.invitingMembers[i].message = errorMessage
+          })
+      })
     }
   },
   created: function () {
