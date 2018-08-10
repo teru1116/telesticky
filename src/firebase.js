4aa5b9083a9d5025e6df93b31138fb2702d333ac
kusumoto <kusumoto@PC10312.local>
2018-08-10 21:49:48 +0900

    会員登録完了でトップ画面遷移

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase'
 import 'firebase/firestore'
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyBInAaDcev5irvlzvWN-3ggmIQQcs3iQpg",
-  authDomain: "web-scrum-board.firebaseapp.com",
-  databaseURL: "https://web-scrum-board.firebaseio.com",
-  projectId: "web-scrum-board",
-  storageBucket: "web-scrum-board.appspot.com",
-  messagingSenderId: "554435278181"
+  apiKey: 'AIzaSyBInAaDcev5irvlzvWN-3ggmIQQcs3iQpg',
+  authDomain: 'web-scrum-board.firebaseapp.com',
+  databaseURL: 'https://web-scrum-board.firebaseio.com',
+  projectId: 'web-scrum-board',
+  storageBucket: 'web-scrum-board.appspot.com',
+  messagingSenderId: '554435278181'
 }
 firebase.initializeApp(config)
 
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,3 +1,47 @@
 <template>
-  <div>Sign Up</div>
+  <div>
+    <input
+      v-model="email"
+      placeholder="メールアドレス"
+    >
+    <input
+      v-model="password"
+      type="password"
+      placeholder="パスワード"
+    >
+    <button
+      @click="signUp"
+    >
+      さっそく使ってみる
+    </button>
+  </div>
 </template>
+
+<script>
+import router from './../router'
+import firebase from './../firebase'
+
+export default {
+  data: function () {
+    return {
+      'email': '',
+      'password': ''
+    }
+  },
+  methods: {
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  },
+  created: function () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        router.push('/teams')
+      }
+    })
+  }
+}
+</script>
