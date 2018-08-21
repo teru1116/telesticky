1fbca610e6ff5f51465b01a2358350f95bcbcd96
kusumoto <kusumoto@PC10312.local>
2018-08-22 00:29:12 +0900

    fixed: md-dialog内がscrollしない

--- src/components/CreateProductBacklogItem.vue
+++ src/components/CreateProductBacklogItem.vue
@@ -4,64 +4,73 @@
       <h2>新規プロダクトバックログアイテム作成</h2>
     </md-toolbar>
     <md-dialog-content>
-      <md-field>
-        <label>タイトル</label>
-        <md-textarea
-          v-model="title"
-          md-autogrow
-        />
-      </md-field>
-      <md-field>
-        <label>見積り</label>
-        <md-input
-          v-model="estimate"
-          type="number"
-          class="estimate"
-        />
-      </md-field>
-      <md-field
-        v-if="!showsDescriptionPreview"
-      >
-        <label>詳細（Markdownで入力できます）</label>
-        <md-textarea
-          v-model="description"
-        >
-        </md-textarea>
-      </md-field>
       <div
-        v-if="showsDescriptionPreview"
-        class="markdown-preview"
+        class="dialog-content-inner"
       >
-        <VueMarkdown
-          :source="description"
-        />
-      </div>
-      <md-button
-        v-if="description"
-        class="md-dense md-primary"
-        @click="showsDescriptionPreview = !showsDescriptionPreview"
-      >
-        {{ showsDescriptionPreview ? '編集' : 'プレビュー' }}
-      </md-button>
-      <md-field>
-        <label>価値</label>
-        <md-textarea
-          v-model="value"
-          md-autogrow
+        <md-field>
+          <label>タイトル</label>
+          <md-textarea
+            v-model="title"
+            md-autogrow
+          />
+        </md-field>
+        <md-field>
+          <label>見積り</label>
+          <md-input
+            v-model="estimate"
+            type="number"
+            class="estimate"
+          />
+        </md-field>
+        <md-field
+          v-if="!showsDescriptionPreview"
         >
-        </md-textarea>
-      </md-field>
-        <!-- TODO: definitions of done -->
-        <!-- TODO: checkbox for Ready -->
-        <!-- TODO: actions: delete save -->
-      </md-field>
+          <label>詳細（Markdownで入力できます）</label>
+          <md-textarea
+            v-model="description"
+          >
+          </md-textarea>
+        </md-field>
+        <div
+          v-if="showsDescriptionPreview"
+          class="markdown-preview"
+        >
+          <VueMarkdown
+            :source="description"
+          />
+        </div>
+        <md-button
+          v-if="description"
+          class="md-dense md-primary"
+          @click="showsDescriptionPreview = !showsDescriptionPreview"
+        >
+          {{ showsDescriptionPreview ? '編集' : 'プレビュー' }}
+        </md-button>
+        <md-field>
+          <label>価値</label>
+          <md-textarea
+            v-model="value"
+            md-autogrow
+          >
+          </md-textarea>
+        </md-field>
+          <!-- TODO: definitions of done -->
+          <!-- TODO: checkbox for Ready -->
+          <!-- TODO: actions: delete save -->
+      </div>
       <md-dialog-actions>
+        <md-button
+          @click="submit"
+          class="md-raised"
+        >
+          削除
+        </md-button>
         <md-button
           @click="submit"
           class="md-raised md-primary"
           :disabled="title.length >= 1"
         >
-          プロダクトバックログに追加する
+          プロダクトバックログに追加
         </md-button>
       </md-dialog-actions>
     </md-dialog-content>
@@ -124,17 +133,37 @@ export default {
   }
 }
 
-.md-dialog-content {
-  width: 600px;
-  padding: 16px 24px 32px;
-  overflow-y: scroll;
+.md-dialog-container {
+  width: 680px;
 
-  .md-input {
+  .md-toolbar {
+    height: 48px;
   }
 
-  .markdown-preview {
-    width: 100%;
-    min-height: 40px;
+  .md-dialog-content {
+    max-height: calc(80vh - 48px);
+    overflow-y: auto;
+    padding: 0;
+
+    .dialog-content-inner {
+      padding: 16px 24px 72px;
+
+      .markdown-preview {
+        width: 100%;
+        min-height: 40px;
+      }
+    }
+
+    .md-dialog-actions {
+      position: fixed;
+      bottom: 0;
+      height: 48px;
+      width: 100%;
+      padding: 0 24px;
+      border-top: 1px solid rgba(0,0,0,0.12);
+      background-color: #fff;
+      z-index: 15;
+    }
   }
 }
 </style>
