9639103c8d28c8a149f5b6a24f7999ca9e9eb88a
kusumoto <kusumoto@PC10312.local>
2018-09-05 14:11:29 +0900

    認証: signUpにユーザー名含むバリデーションを実装

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,28 +1,57 @@
 <template>
   <div>
+    <header>
+      <h1>
+        Scrum
+      </h1>
+    </header>
+
+    <h2>
+      さっそくScrumをはじめよう！
+    </h2>
+    <p>
+      スクラムは、経験的プロセス制御の理論（経験主義）を基本にしている。<br />
+      経験主義とは、実際の経験と既知に基づく判断によって知識が獲得できるというものである。<br />
+      スクラムでは、反復的かつ漸進的な手法を用いて、予測可能性の最適化とリスクの管理を行う。
+    </p>
 
     <ul
       class="form-items"
     >
       <li>
         <input
+          v-model="displayName"
+          type=text
+          placeholder="ユーザー名"
+        />
+        <span
+          class="error">
+        {{ displayNameError }}
+        </span>
+      </li>
+      <li>
+        <input
           v-model="email"
           type=email
+          placeholder="メールアドレス"
         />
+        <span
+          class="error">
+        {{ emailError }}
+        </span>
       </li>
       <li>
         <input
           v-model="password"
           type=password
+          placeholder="パスワード"
         />
+        <span
+          class="error">
+        {{ passwordError }}
+        </span>
       </li>
     </ul>
-
-    <span
-      v-if="errorMessage"
-    >
-      {{ errorMessage }}
-    </span>
 
     <md-button
       @click="signup"
@@ -46,25 +75,16 @@ import firebase from './../firebase'
 const db = firebase.firestore()
 
 export default {
-  computed: {
-    errorMessage: function () {
-      switch (this.firebaseErrorCode) {
-        case 'auth/email-already-in-use':
-          return 'すでに使われているメールアドレスです。'
-        case 'auth/invalid-email':
-          return 'メールアドレスの形式が無効です。'
-        case 'auth/weak-password':
-          return '入力されたパスワードが簡単すぎます。'
-        default:
-          return ''
-      }
-    }
-  },
   data: function () {
     return {
+      // 入力項目
+      'displayName': '',
       'email': '',
       'password': '',
-      'firebaseErrorCode': ''
+      // エラー
+      'displayNameError': '',
+      'emailError': '',
+      'passwordError': ''
     }
   },
   methods: {
@@ -72,6 +92,15 @@ export default {
       'setAuthUser'
     ]),
     signup: function () {
+      // ユーザー名バリデーション
+      const regex = /^[a-zA-Z0-9_\-.]{3,15}/
+      if (!regex.test(this.displayName)) {
+        this.displayNameError = 'ユーザー名は3文字から15文字で、半角英数字、ハイフン、ドット、アンダースコアが使用できます。'
+        return
+      } else {
+        this.displayNameError = ''
+      }
+      // 会員登録API
       firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
         .then(userCredential => {
           // authUserをstoreにセット
@@ -87,7 +116,18 @@ export default {
           router.push('/teams')
         })
         .catch(error => {
-          this.firebaseErrorCode = error.code
+          switch (error.code) {
+            case 'auth/email-already-in-use':
+              this.emailError = 'すでに使われているメールアドレスです。'
+              return
+            case 'auth/invalid-email':
+              this.emailError =  'メールアドレスの形式が無効です。'
+              return
+            case 'auth/weak-password':
+              this.passwordError = '入力されたパスワードが簡単すぎます。'
+            default:
+              return
+          }
         })
     }
   }
