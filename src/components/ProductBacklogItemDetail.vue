056922af403fa0a59a19e9eb4a88f87a3ca39301
kusumoto <kusumoto@PC10312.local>
2018-09-13 17:13:36 +0900

    アイテム詳細: 表示とtextareaが切り替わるUIを作成

--- src/components/ProductBacklogItemDetail.vue
+++ src/components/ProductBacklogItemDetail.vue
@@ -1,15 +1,45 @@
 <template>
-  <md-button
-    @click="$router.push({ name: 'productBacklog' })"
-  >
-    <md-icon>clear</md-icon>
-  </md-button>
+  <div>
+    <md-button
+      @click="$router.push({ name: 'productBacklog' })"
+    >
+      <md-icon>clear</md-icon>
+    </md-button>
+    <ul>
+      <!-- タイトル -->
+      <li>
+        <h3>タイトル</h3>
+        <p
+          v-if="editingForm !== 'title'"
+          @click="editingForm = 'title'"
+        >
+          {{ item.title }}
+        </p>
+        <textarea
+          v-if="editingForm === 'title'"
+          v-model="title"
+          @blur="updateItem('title', title)"
+        />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     item: Object
+  },
+  data () {
+    return {
+      title: this.item.title,
+      editingForm: ''
+    }
+  },
+  methods: {
+    updateItem (field, value) {
+      console.log(field, value)
+    }
   }
 }
 </script>
