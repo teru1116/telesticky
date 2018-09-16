1ed726110b5964281601cb798ba28a0b542869bf
kusumoto <kusumoto@PC10312.local>
2018-09-16 17:57:34 +0900

    members: ステートをmd-tableを使って表示

--- src/components/TeamMembers.vue
+++ src/components/TeamMembers.vue
@@ -2,7 +2,33 @@
   <div
     class="inner"
   >
-    <!-- -->
+    <!-- header -->
+    <div
+      class="content-header"
+    >
+      <h2>
+        メンバー
+      </h2>
+      <div
+        class="header-items"
+      >
+        <md-button class="md-raised md-primary primary-button">
+          <md-icon>add</md-icon>
+          招待する
+        </md-button>
+      </div>
+    </div>
+
+    <!-- table -->
+    <div
+      class="members-table"
+    >
+      <md-table v-model="members" md-card>
+        <md-table-row slot="md-table-row" slot-scope="{ item }">
+          <md-table-cell md-label="メールアドレス">{{ item.email }}</md-table-cell>
+        </md-table-row>
+      </md-table>
+    </div>
   </div>
 </template>
 
@@ -28,3 +54,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.members-table {
+  margin: 16px 0 0
+}
+</style>
