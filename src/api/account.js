3137657d90e368cd8c97120831e65333855066ad
kusumoto <kusumoto@PC10312.local>
2018-09-28 10:35:33 +0900

    awaitを複数回使う場合は、await/catchパターンだと都度catchを書かなければいけないので、try/catchを使うことにした

--- src/api/account.js
+++ src/api/account.js
@@ -1,69 +1,27 @@
 import firebase from '@/firebase'
-
 const db = firebase.firestore()
 
 export default {
-  updateDisplayName (teamId, uid, displayName) {
-    // Authを更新
-    firebase.auth().currentUser.updateProfile({
-      displayName: displayName
-    })
-
-    // Databaseを更新
-    const batch = db.batch()
-    batch.update(db.collection('users').doc(uid), {
-      displayName: displayName
-    })
-    batch.update(db.collection('scrumTeams').doc(teamId).collection('members').doc(uid), {
-      displayName: displayName
-    })
-
-    return new Promise((resolve, reject) => {
-      batch.commit()
-        .then(() => {
-          resolve()
-        })
-        .catch(error => {
-          reject(error)
-        })
-    })
+  async updateDisplayName (uid, displayName) {
+    try {
+      await firebase.auth().currentUser.updateProfile({ displayName })
+      await db.collection('users').doc(uid).update({ displayName })
+    } catch (error) {
+      throw new Error(error)
+    }
   },
 
-  updateProfilePhoto (teamId, uid, dataURL) {
-    return new Promise((resolve, reject) => {
+  async updateProfilePhoto (uid, dataURL) {
+    try {
       // 画像アップロード
       const ref = firebase.storage().ref().child(`profile/${uid}/small/photo.jpg`)
-      ref.putString(dataURL, 'data_url')
-        .then(snapshot => {
-          return ref.getDownloadURL()
-        })
-        .then(url => {
-          // Authを更新
-          firebase.auth().currentUser.updateProfile({
-            photoURL: url
-          })
-
-          // Databaseを更新
-          const batch = db.batch()
-          batch.update(db.collection('users').doc(uid), {
-            photoURL: url
-          })
-          batch.set(db.collection('scrumTeams').doc(teamId).collection('members').doc(uid), {
-            photoURL: url
-          })
-
-          batch.commit()
-            .then(() => {
-              resolve(url)
-            })
-            .catch(error => {
-              console.error('batch error', error)
-              reject(error)
-            })
-        })
-        .catch(error => {
-          console.error(error)
-        })
-    })
+      await ref.putString(dataURL, 'data_url')
+      // 画像URLをAuthとDatabaseにセット
+      const photoURL = await ref.getDownloadURL()
+      await firebase.auth().currentUser.updateProfile({ photoURL })
+      await db.collection('users').doc(uid).update({ photoURL })
+    } catch (error) {
+      throw new Error(error)
+    }   
   }
 }
