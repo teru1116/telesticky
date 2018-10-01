f89e7dcf8a8739574f28d45b61513ef4fa8564d6
kusumoto <kusumoto@PC10312.local>
2018-10-01 17:11:35 +0900

    firebaseのwarning消し

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import 'firebase/firestore'
 
-// Initialize Firebase
-const config = {
+firebase.initializeApp({
   apiKey: 'AIzaSyBInAaDcev5irvlzvWN-3ggmIQQcs3iQpg',
   authDomain: 'web-scrum-board.firebaseapp.com',
   databaseURL: 'https://web-scrum-board.firebaseio.com',
   projectId: 'web-scrum-board',
   storageBucket: 'web-scrum-board.appspot.com',
   messagingSenderId: '554435278181'
-}
-firebase.initializeApp(config)
+})
 
 export default firebase
